22b65b2d09f0703014b3292ed7c7a6c72ad69467
Dani <da.picarte@duocuc.cl>
2026-05-25 12:25:14 -0400

    check

--- src/main/java/BookPoint/autentificacion/service/AutentificacionService.java
+++ src/main/java/BookPoint/autentificacion/service/AutentificacionService.java
@@ -1,7 +1,6 @@
 package BookPoint.autentificacion.service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
